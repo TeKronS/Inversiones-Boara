22365a214f2ce733515ef51bb6989a175f17b50b
TeKron <asper.tk93@gmail.com>
2022-02-23 15:52:01 -0400

    Configuto el PWA

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import { register } from './serviceWorkerRegistration';
+
 
 import { App } from "./App";
 
@@ -11,3 +13,6 @@ ReactDOM.render(
   </BrowserRouter>,
   rootElement
 );
+
+
+register();
