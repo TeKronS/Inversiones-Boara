59b9ecce93a330207c1f940708a003995f12a4a6
TeKron <asper.tk93@gmail.com>
2022-03-20 19:33:03 -0400

    edito if del fetch request

--- src/service-worker.js
+++ src/service-worker.js
@@ -74,13 +74,14 @@ self.addEventListener("message", (event) => {
 
 self.addEventListener("fetch", (event) => {
   if (
-    event.url.includes(".jpg") ||
-    event.url.includes(".jpeg") ||
-    event.url.includes(".png")
+    event.request.url.includes(".jpg") ||
+    event.request.url.includes(".jpeg") ||
+    event.request.url.includes(".png")
   ) {
     event.respondWith(
       caches.match(event.request).then(function (response) {
         // Cache hit - return response
+
         if (response) {
           return response;
         }
@@ -89,15 +90,11 @@ self.addEventListener("fetch", (event) => {
         // can only be consumed once. Since we are consuming this
         // once by cache and once by the browser for fetch, we need
         // to clone the response.
+
         const fetchRequest = event.request.clone();
 
         return fetch(fetchRequest).then(function (response) {
-          // Check if we received a valid response
-          if (
-            !response ||
-            response.status !== 200 ||
-            response.type !== "basic"
-          ) {
+          if (!response) {
             return response;
           }
 
@@ -105,15 +102,18 @@ self.addEventListener("fetch", (event) => {
           // and because we want the browser to consume the response
           // as well as the cache consuming the response, we need
           // to clone it so we have two streams.
+
           const responseToCache = response.clone();
 
           caches.open("Imagenes").then(function (cache) {
             cache.put(event.request, responseToCache);
           });
-
           return response;
         });
       })
     );
   }
 });
+
+{
+}
